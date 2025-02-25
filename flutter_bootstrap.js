a1f7063f7360a1b85995bc01aab29a38a216731d
Oghenetega Eko-Brotobor <otegabrotobor@gmail.com>
2025-02-25 14:28:39 +0100

    final changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"4d08217f8dca7b0564decc82f22fcb2becf7b5
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4087875365"
+    serviceWorkerVersion: "2097599656"
   }
 });
